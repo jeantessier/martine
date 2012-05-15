ee648c1484f60706e4e9a004f4f391d5139967ab
Jean Tessier <jean@jeantessier.com>
2012-05-14 23:10:08 -0700

    Install the sprint-security-core plugin.  * Create User and Role classes.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -91,3 +91,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.martine.user.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.martine.user.UserRole'
+grails.plugins.springsecurity.authority.className = 'com.martine.user.Role'
